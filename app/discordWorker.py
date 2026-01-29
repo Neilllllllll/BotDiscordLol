d0937eedf99a5b159020084d6813039c61d3226b
Neil <neil.rabia972972@gmail.com>
2026-01-29 10:02:00 +0100

    Ajout du service discord avec message récursif

--- app/discordWorker.py
+++ app/discordWorker.py
@@ -1,4 +1,16 @@
-from app.services.discord-service import DiscordService
-from app.services.bdd-service import BDDService
-# Logique pour écrire dans un canal Discord
+from app.services import DiscordService
+from app.services import BddService
+from app.config.config import DISCORD_TOKEN, CHANNEL_ID, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT
 
+if __name__ == "__main__":
+    # Initialisation des services de la BDD et du bot Discord
+    bdd = BddService(host="db", database="botdb", user="user", password="password", port=5432)
+    discord_bot = DiscordService(DISCORD_TOKEN, bdd, CHANNEL_ID)
+    
+    # Démarrage du bot
+    try:
+        print("Démarrage du bot Discord...")
+        discord_bot.run()
+    except KeyboardInterrupt:
+        bdd.close()
+    print("Bot arrêté.")
